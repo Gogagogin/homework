3f4f2322b79a346ef1509793247e84399c7c5011
Gogagogin <georgec.gil@gmail.com>
2020-02-27 23:28:15 +0300

    Thirteenth Task

---
+++ Task13/src/by/gil/Main.java
@@ -0,0 +1,18 @@
+package by.gil;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "Число простое";
+
+        int number = 491;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                a = "Число не простое, а имеет как минимум делитель :" + i;
+                break;
+            }
+        }
+
+        System.out.println(a);
+    }
+}
