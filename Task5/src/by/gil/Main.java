b36a3beee3fa6e364e887474169f318cdcc7289a
Gogagogin <georgec.gil@gmail.com>
2020-02-27 21:42:25 +0300

    Fifth task

--- Task5/src/by/gil/Main.java
+++ Task5/src/by/gil/Main.java
@@ -3,7 +3,7 @@ package by.gil;
 public class Main {
 
     public static void main(String[] args) {
-        String rub = null;
+        String rub; //null
         for (int i = 1; i <= 10; i++) {
             int s = (int) (Math.random() * 1000000);
             if (s % 10 == 1) {
@@ -11,6 +11,9 @@ public class Main {
             } else if (s % 10 > 1 && s % 10 < 4) {
                 rub = " рубля";
             } else if ((s % 10 > 4 && s % 10 <= 9) || (s % 100 > 10 && s % 100 < 21) || s % 10 == 0) {
+                /* Maybe it’s not the best way to solve such problem, and even variant with
+                ( (s % 10 > 4 && s % 10 <= 9) || (s % 100 >= 10 && s % 100 < 21)) is better, but it works)
+                 */
                 rub = " рублей";
             }
             System.out.println(s + rub);
