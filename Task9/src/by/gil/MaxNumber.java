b2be079b527dca0b39f01b4dcb40245d7b55c7b3
Gogagogin <georgec.gil@gmail.com>
2020-02-27 21:57:54 +0300

    Ninth Task

---
+++ Task9/src/by/gil/MaxNumber.java
@@ -0,0 +1,24 @@
+package by.gil;
+
+public class MaxNumber {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int i = 1;
+        int x = (int) (Math.random() * 50);
+        while (i < 20) {
+            if (a < x) {
+                System.out.print(" X= " + x + " =X "); // " X= " + x + " =X " is not really necessary,
+                                                                // but it’s help us to see the process
+                                                                // and ofcorse, i now about array search
+                                                                //but this task is before arrays
+                a = x;
+            } else {
+                x = (int) (Math.random() * 20);
+                System.out.print(x + " ");
+            }
+            i++;
+        }
+        System.out.println("\n Максимальное :" + a);
+    }
+}
