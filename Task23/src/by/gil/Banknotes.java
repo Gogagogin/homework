b48a23d985e439f316a360266c303588a67e2f6d
Gogagogin <georgec.gil@gmail.com>
2020-03-30 00:02:46 +0300

    Twenty Third

---
+++ Task23/src/by/gil/Banknotes.java
@@ -0,0 +1,90 @@
+package by.gil;
+
+public class Banknotes {
+
+    private int numberOfTwenty;
+    private int numberOfFifty;
+    private int numberOfHundred;
+
+    public Banknotes(int numberOfTwenty, int numberOfFifty, int numberOfHundred) {
+        this.numberOfTwenty = numberOfTwenty;
+        this.numberOfFifty = numberOfFifty;
+        this.numberOfHundred = numberOfHundred;
+    }
+
+    public int addTwenty(int inputNumberOfTwenty) {
+        this.numberOfTwenty += inputNumberOfTwenty;
+        return this.numberOfTwenty;
+    }
+
+    public int addFifty(int inputNumberOfFifty) {
+        this.numberOfFifty += inputNumberOfFifty;
+        return this.numberOfFifty;
+    }
+
+    public int addHundred(int inputNumberOfHundred) {
+
+        return this.numberOfHundred + inputNumberOfHundred;
+    }
+/*
+    public int getNumberOfTwenty()
+    {
+        return numberOfTwenty;
+    }
+
+    public int getNumberOfFifty() {
+
+        return numberOfFifty;
+    }
+
+    public int getNumberOfHundred()
+    {
+        return numberOfHundred;
+    }*/
+
+    public void getSum() {
+        System.out.println("В банкомате сейчас " + ((numberOfHundred * 100) + (numberOfFifty * 50) + (numberOfTwenty * 20)) + " Минимальная купюра 20");
+    }
+
+
+    public boolean extraditionOfMoney(int requestedSum) {
+
+        if (requestedSum > (numberOfFifty * 50 + numberOfHundred * 100 + numberOfTwenty * 20)) {
+            System.out.println(" У меня нет столько денег, простите пожалуйста");
+            return false;
+        }
+        if (requestedSum % 10 != 0 || requestedSum == 30 || requestedSum == 10) {
+            System.out.println(" Я не могу выдать такую сумму, простите");
+            return false;
+        }
+
+        int uotPutNumberOfHundred = requestedSum / 100;
+        if (this.numberOfHundred < uotPutNumberOfHundred) {
+            uotPutNumberOfHundred = this.numberOfHundred;
+        }
+        int uotPutNumberOfFifty = (requestedSum - 100 * uotPutNumberOfHundred) / 50;
+        if (this.numberOfFifty < uotPutNumberOfFifty) {
+            uotPutNumberOfFifty = this.numberOfFifty;
+        }
+        int uotPutNumberOfTwenty = (requestedSum - 100 * uotPutNumberOfHundred - 50 * uotPutNumberOfFifty) / 20;
+        if (this.numberOfTwenty < uotPutNumberOfTwenty) {
+            uotPutNumberOfTwenty = this.numberOfTwenty;
+        }
+        if (this.numberOfHundred == 0 || this.numberOfFifty == 0 || requestedSum % 20 != 0) {
+
+            System.out.println(" Я не могу выдать такую сумму, простите");
+            return false;
+
+        } else {
+            System.out.println(uotPutNumberOfHundred + " Х 100 " +
+                    uotPutNumberOfFifty + " Х 50 " +
+                    uotPutNumberOfTwenty + " Х 20 ");
+            this.numberOfTwenty -= uotPutNumberOfTwenty;
+            this.numberOfFifty -= uotPutNumberOfFifty;
+            this.numberOfHundred -= uotPutNumberOfHundred;
+
+
+            return true;
+        }
+    }
+}
---
+++ Task23/src/by/gil/Bankomat.java
@@ -0,0 +1,80 @@
+package by.gil;
+
+import java.util.Scanner;
+
+public class Bankomat {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Введите колчество купюр в банкомате: ");
+        Scanner in20 = new Scanner(System.in);
+        System.out.println("Количество купюр номиналом 20 :");
+        int paper20 = in20.nextInt();
+        Scanner in50 = new Scanner(System.in);
+        System.out.println("Количество купюр номиналом 50 :");
+        int paper50 = in50.nextInt();
+        Scanner in100 = new Scanner(System.in);
+        System.out.println("Количество купюр номиналом 100 :");
+        int paper100 = in100.nextInt();
+
+        Banknotes bankomat = new Banknotes(paper20, paper50, paper100);
+
+        Scanner operation = new Scanner(System.in);
+
+
+        while (true) {
+
+            System.out.println("Выберите операцию: \n 1 - пополнение; \n 2 - снятие наличных; \n 3 - просмотр суммы в банкомате; \n 4 - выйти;");
+            int numberOfOperation = operation.nextInt();
+            if (numberOfOperation == 1) {
+                while (true) {
+                    Scanner operationReplenishment = new Scanner(System.in);
+                    System.out.println("какую купюры вы хотите вставить в банкомат: \n 1 - 20; \n 2 - 50; \n 3 - 100; \n 4 - выход;");
+                    int numberOfOperationReplenishment = operationReplenishment.nextInt();
+                    if (numberOfOperationReplenishment == 1) {
+                        Scanner replenishmentTwenty = new Scanner(System.in);
+                        System.out.println("сколько купюр номиналом 20 вы вносите ?");
+                        int inputNumberOfTwenty = replenishmentTwenty.nextInt();
+                        bankomat.addTwenty(inputNumberOfTwenty);
+                    } else if (numberOfOperationReplenishment == 2) {
+                        Scanner replenishmentFifty = new Scanner(System.in);
+                        System.out.println("сколько купюр номиналом 50 вы вносите ?");
+                        int inputNumberOfFifty = replenishmentFifty.nextInt();
+                        bankomat.addFifty(inputNumberOfFifty);
+                    } else if (numberOfOperationReplenishment == 3) {
+                        Scanner replenishmentFifty = new Scanner(System.in);
+                        System.out.println("сколько купюр номиналом 100 вы вносите ?");
+                        int inputNumberOfHundred = replenishmentFifty.nextInt();
+                        bankomat.addHundred(inputNumberOfHundred);
+                    } else if (numberOfOperationReplenishment == 4) {
+                        break;
+                    } else {
+                        System.out.println("Error");
+                    }
+                }
+            }
+            if (numberOfOperation == 2) {
+
+                Scanner extraditionOfMoney = new Scanner(System.in);
+                bankomat.getSum();
+                System.out.println("Какую сумму вам выдать ?");
+                int summa = extraditionOfMoney.nextInt();
+                bankomat.extraditionOfMoney(summa);
+                bankomat.getSum();
+            }
+
+            if (numberOfOperation == 3) {
+                bankomat.getSum();
+            }
+
+            if (numberOfOperation == 4) {
+                break;
+            } else {
+                System.out.println("Error");
+            }
+        }
+    }
+}
+
+
+
